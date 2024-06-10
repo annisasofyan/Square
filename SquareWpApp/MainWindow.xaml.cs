5ed958043b26432e9259dd314a4db8a13ac2ddab
annisasofyan <annisa.sofyan30@gmail.com>
2024-06-10 22:15:47 +0700

    login dan ganti warna sudah bisa

--- SquareWpApp/MainWindow.xaml.cs
+++ SquareWpApp/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,27 +26,37 @@ namespace SquareWpApp
     public partial class MainWindow : Window
     {
         private static readonly HttpClient client = new HttpClient();
+        private string idToken;
         public MainWindow()
         {
             InitializeComponent();
+            UpdateUIForLoggedOutState();
         }
 
         private async void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             string username = UsernameTextBox.Text;
             string password = PasswordBox.Password;
-
-
-            var loginResult = await LoginAsync(username, password);
-
-            if (loginResult != null && loginResult.Status == 200)
+            try
             {
-                MessageBox.Show("Login berhasil!", "Sukses", MessageBoxButton.OK, MessageBoxImage.Information);
-                // Anda bisa menambahkan logika untuk membuka jendela lain atau melakukan aksi lainnya
+                var loginResult = await LoginAsync(username, password);
+
+                if (loginResult != null && loginResult.Status == 200)
+                {
+                    idToken = loginResult.IdToken;
+                    MessageBox.Show("Login berhasil!", "Sukses", MessageBoxButton.OK, MessageBoxImage.Information);
+                    UpdateUIForLoggedInState();
+                }
+                else
+                {
+                    MessageBox.Show("Login gagal: " + loginResult?.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    UpdateUIForLoggedOutState();
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("Login gagal: " + loginResult?.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                UpdateUIForLoggedOutState();
             }
 
         }
@@ -72,6 +83,66 @@ namespace SquareWpApp
             return await response.Content.ReadFromJsonAsync<LoginResponse>();
         }
 
+        private  async void ChangeColorButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(idToken))
+            {
+                MessageBox.Show("Silakan login terlebih dahulu.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
 
+            try
+            {
+                var colorResult = await GetRandomSquareColorAsync(idToken);
+
+                if (colorResult != null && colorResult.Status == 200)
+                {
+                    ColorSquare.Fill = new SolidColorBrush((Color)ColorConverter.ConvertFromString(colorResult.Result.Color));
+                }
+                else
+                {
+                    MessageBox.Show("Gagal mendapatkan warna: " + colorResult?.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+        private async Task<SquareColorResponse> GetRandomSquareColorAsync(string token)
+        {
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+
+            HttpResponseMessage response;
+            try
+            {
+                response = await client.GetAsync("https://squareapi.numpang.my.id/api/Square/Random");
+                response.EnsureSuccessStatusCode();
+            }
+            catch (HttpRequestException ex)
+            {
+                throw new Exception("Error contacting the API: " + ex.Message);
+            }
+
+            return await response.Content.ReadFromJsonAsync<SquareColorResponse>();
+        }
+
+
+        private void UpdateUIForLoggedInState()
+        {
+            ChangeColorButton.IsEnabled = true;
+            ColorSquare.Fill = new SolidColorBrush(Colors.Gray);
+            StatusTextBlock.Text = "You can now change the square's colour.";
+            StatusTextBlock.Foreground = new SolidColorBrush(Colors.Black);
+        }
+
+        private void UpdateUIForLoggedOutState()
+        {
+            ChangeColorButton.IsEnabled = false;
+            ColorSquare.Fill = new SolidColorBrush(Colors.Gray);
+            StatusTextBlock.Text = "Please log in first to change the square's colour.";
+            StatusTextBlock.Foreground = new SolidColorBrush(Colors.Gray);
+            idToken = null;
+        }
     }
 }
---
+++ SquareWpApp/Model/SquareColorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareWpApp.Model
+{
+    internal class SquareColorResponse
+    {
+        public int Status { get; set; }
+        public SquareColorResult Result { get; set; }
+        public string Message { get; set; }
+        public object Errors { get; set; }
+    }
+}
