72a45db642b8fdf09d098e49caa06d6ce1abb282
annisasofyan <annisa.sofyan30@gmail.com>
2024-06-13 20:30:37 +0700

    gambar udah bisa dipanggil

--- WeatherCity/MainWindow.xaml.cs
+++ WeatherCity/MainWindow.xaml.cs
@@ -30,35 +30,35 @@ namespace WeatherCity
             InitializeComponent();
         }
 
-        private async void FetchWeatherButton_Click(object sender, RoutedEventArgs e)
-        {
-            //string city = (CityComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
-            //if (string.IsNullOrEmpty(city))
-            //{
-            //    MessageBox.Show("Please select a city.");
-            //    return;
-            //}
+        //private async void FetchWeatherButton_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //string city = (CityComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+        //    //if (string.IsNullOrEmpty(city))
+        //    //{
+        //    //    MessageBox.Show("Please select a city.");
+        //    //    return;
+        //    //}
 
-            //string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={ApiKey}&units=metric";
+        //    //string apiUrl = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={ApiKey}&units=metric";
 
-            //try
-            //{
-            //    string response = await Client.GetStringAsync(apiUrl);
-            //    dynamic weatherData = JObject.Parse(response);
+        //    //try
+        //    //{
+        //    //    string response = await Client.GetStringAsync(apiUrl);
+        //    //    dynamic weatherData = JObject.Parse(response);
 
-            //    WeatherConditionTextBlock.Text = $"Condition: {weatherData.weather[0].main}";
-            //    TemperatureTextBlock.Text = $"Temperature: {weatherData.main.temp} °C";
-            //    HumidityTextBlock.Text = $"Humidity: {weatherData.main.humidity} %";
-            //    WindSpeedTextBlock.Text = $"Wind Speed: {weatherData.wind.speed} m/s";
-            //}
-            //catch (Exception ex)
-            //{
-            //    MessageBox.Show($"Error fetching weather data: {ex.Message}");
-            //}
-            WeatherConditionTextBlock.Text = $"Condition: Winter";
-            TemperatureTextBlock.Text = $"Temperature: 7°C";
-            HumidityTextBlock.Text = $"Humidity: 60%";
-            WindSpeedTextBlock.Text = $"Wind Speed: 70 m/s";
-        }
+        //    //    WeatherConditionTextBlock.Text = $"Condition: {weatherData.weather[0].main}";
+        //    //    TemperatureTextBlock.Text = $"Temperature: {weatherData.main.temp} °C";
+        //    //    HumidityTextBlock.Text = $"Humidity: {weatherData.main.humidity} %";
+        //    //    WindSpeedTextBlock.Text = $"Wind Speed: {weatherData.wind.speed} m/s";
+        //    //}
+        //    //catch (Exception ex)
+        //    //{
+        //    //    MessageBox.Show($"Error fetching weather data: {ex.Message}");
+        //    //}
+        //    WeatherConditionTextBlock.Text = $"Condition: Winter";
+        //    TemperatureTextBlock.Text = $"Temperature: 7°C";
+        //    HumidityTextBlock.Text = $"Humidity: 60%";
+        //    WindSpeedTextBlock.Text = $"Wind Speed: 70 m/s";
+        //}
     }
 }
